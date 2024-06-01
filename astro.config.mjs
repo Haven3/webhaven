9760390aa503fdea5f1387caa55b94e9019fac18
Jason Haven <jason@jasonhaven.com>
2024-06-01 11:51:14 -0500

    init commit

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from "astro/config";
+
+import netlify from '@astrojs/netlify';
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  adapter: netlify(),
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
+});
